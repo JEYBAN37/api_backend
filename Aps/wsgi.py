2381214faf9b10d249d12303d0c5771fc63b7477
JEYBAN37 <jeyban37@gmail.com>
2024-07-19 23:03:45 -0500

    fix asinc normal

--- Aps/wsgi.py
+++ Aps/wsgi.py
@@ -1,30 +1,22 @@
-"""
-WSGI config for Aps project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 import threading
-
+import asyncio
 from django.core.wsgi import get_wsgi_application
-
-from aps_api.signals.method_generator import daily_code
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aps.settings')
 application = get_wsgi_application()
 
-import asyncio
+from aps_api.signals.method_generator import daily_code
 
 
 def start_background_tasks():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.create_task(daily_code())
-    loop.run_forever()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.close()
 
 
 threading.Thread(target=start_background_tasks, daemon=True).start()
